ad8bfc28591f96ba643ab4f7881c5b1f52923b57
brucengdev <brucengdev@gmail.com>
2024-12-25 23:27:54 +0700

    Update test

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,11 +1,12 @@
 import { screen, render } from "@testing-library/react";
 import App from "../App";
 import {describe, expect, it} from 'vitest'
+import '@testing-library/jest-dom'
 
 describe("App", () => {
     it("displays a header", () => {
         render(<App />)
 
-        expect(screen.getByRole("heading", { name: "Vite + React"})).toBeTruthy()
+        expect(screen.getByRole("heading", { name: "Vite + React"})).toBeInTheDocument()
     })
 })
